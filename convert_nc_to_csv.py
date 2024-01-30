9b932c26e44fc0bfe49ac9ec6a754805742eec98
ediakatos <ediakatos@mapaction.org>
2024-01-30 19:06:16 +0000

    Format with black and fix flake8 warnings in convert_nc_to_csv.py

--- convert_nc_to_csv.py
+++ convert_nc_to_csv.py
@@ -1,13 +1,12 @@
 import xarray as xr
-import pandas as pd
 import os
 
 # Define the directory where your .nc files are located
-data_directory = os.path.expanduser('~/Downloads/ECMWF_Test_dir')
+data_directory = os.path.expanduser("~/Downloads/ECMWF_Test_dir")
 
 # Loop through the .nc files in the directory
 for filename in os.listdir(data_directory):
-    if filename.endswith('.nc'):
+    if filename.endswith(".nc"):
         filepath = os.path.join(data_directory, filename)
         dataset = xr.open_dataset(filepath)
 
@@ -15,10 +14,10 @@ for filename in os.listdir(data_directory):
         df = dataset.to_dataframe().reset_index()
 
         # Determine the output CSV filename based on the input filename
-        csv_filename = os.path.splitext(filename)[0] + '.csv'
+        csv_filename = os.path.splitext(filename)[0] + ".csv"
         csv_filepath = os.path.join(data_directory, csv_filename)
 
         # Export the DataFrame to the CSV file
         df.to_csv(csv_filepath, index=False)
 
-        print(f'Data exported to {csv_filepath}')
+        print(f"Data exported to {csv_filepath}")
